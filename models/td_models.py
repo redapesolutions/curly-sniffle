93763c814b96d28b21c0944a4ee5da513d913f25
MAC OS <macos@MACs-MacBook-Pro.local>
2016-10-30 23:12:51 +0500

    Sunday commit, some ideas

---
+++ models/td_models.py
@@ -0,0 +1,20 @@
+""" Classes for OOP
+author: Akzhol
+"""
+
+class Driver(object):
+	"""Driver object"""
+	def __init__(self, name, surname, email, dob):
+
+
+class Vehicle(object):
+	"""Vehicle is a parent object"""
+
+class Car(Vehicle):
+	"""Car object, child of Vehicle"""
+
+class Cargo(Vehicle):
+	"""Cargo object, for transportation, child of Vehicle"""
+
+class Trip(object):
+	
